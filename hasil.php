5c7fe52052757b0a1584129cf07fca702cd0c86c
arizalmuhamadridwan <ariz23202si@student.nurulfikri.ac.id>
2024-03-05 07:47:19 +0700

    masukan data baru

---
+++ hasil.php
@@ -0,0 +1,45 @@
+<?php
+$proses = $_POST["proses"];
+$nama = $_POST["nama"];
+$matkul = $_POST["matkul"];
+$uts = $_POST["uts"];
+$uas = $_POST["uas"];
+$praktikum = $_POST["praktikum"];
+$rata_rata = ($uts+$uas+$praktikum)/3;
+$keterangan = keterangan($rata_rata);
+$grade = grade($rata_rata);
+
+function keterangan($rata_rata){
+if ($rata_rata >= 55 && $rata_rata <=100) {
+    return "Anda lulus";
+} else {
+    return "masukan angka dengan benar";
+}
+}
+
+function grade($rata_rata){
+if ($rata_rata >= 85 && $rata_rata <=100) {
+    return "A (Sangat baik)";
+} elseif ($rata_rata >= 70 && $rata_rata <=84) {
+    return "B (baik)";
+} elseif ($rata_rata >= 56 && $rata_rata <=69) {
+    return "C (Cukup)";
+}  elseif ($rata_rata >= 36 && $rata_rata <=55) {
+    return "D (Kurang)";
+} elseif ($rata_rata >= 0 && $rata_rata <=35) {
+    return "E (Sangat Kurang)";
+}else{
+    return "I (Tidak Nilai)";
+}
+}
+
+echo "Proses: $proses";
+echo "<br>Nama Lengkap: $nama";
+echo "<br>Mata Kuliah: $matkul";
+echo "<br>Nilai uts: $uts";
+echo "<br>Nilai uas: $uas";
+echo "<br>Nilai praktikum: $praktikum";
+echo "<br>Rata rata nilai: $rata_rata";
+echo "<br>Keterangan: $keterangan";
+echo "<br>Grade: $grade";
+?>
